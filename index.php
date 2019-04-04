4a3a140219207c9bccd6f7705f54bdbb0df47f17
Burhan012 <21abdiburhan@gmail.com>
2019-04-04 16:58:35 +0700

    siap

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    echo "LUG";
+    echo "<br>";
+    print "LUG";
+    echo "<br>";
+    print_r("LUG");
+    echo "<br>";
+    var_dump("buburrrrrr");
+    
+    ?>
+    <h1>LUG <?php echo "stikom" ?></h1>
+    <?php 
+    $a=4;
+    $d=2;
+
+    echo "<h1>Hasil adalah</h1> ".$a/$d."<br>";
+    echo "Hasil modulus adalah ".$a%$d;
+    ?>
+    <?php 
+    $a ="hayyy";
+    $b = "yaaa";
+
+    echo $a.$b;
+    
+    ?>
+
+</body>
+</html>
